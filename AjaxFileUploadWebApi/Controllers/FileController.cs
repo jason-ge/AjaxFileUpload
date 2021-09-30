fb36b119ce9fd5a07aa6061e793a4f989751fc93
wnms2 <wnms2@LAPTOP-IS9ML9MO>
2021-09-30 10:03:01 -0400

    Add project files.

---
+++ AjaxFileUploadWebApi/Controllers/FileController.cs
@@ -0,0 +1,49 @@
+﻿using AjaxFileUploadWebApi.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxFileUploadWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FileController : ControllerBase
+    {
+        private readonly ILogger<FileController> _logger;
+        private IWebHostEnvironment _hostingEnvironment;
+        public FileController(ILogger<FileController> logger, IWebHostEnvironment environment)
+        {
+            _hostingEnvironment = environment;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string HealthCheck()
+        {
+            return "Good to go!";
+        }
+
+        [HttpPost("Upload")]
+        public async Task<AjaxResultcs> Upload(IFormFile file)
+        {
+            if (file != null)
+            {
+                // Make sure the uploadFolder grants everyone Modify permission for testing purpose.
+                string uploadFolder = Path.Combine(_hostingEnvironment.ContentRootPath, "uploads");
+                string filePath = Path.Combine(uploadFolder, file.FileName);
+                using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    await file.CopyToAsync(fileStream);
+                }
+            }
+
+            return new AjaxResultcs() { IsSuccessful = true, Message = "" };
+        }
+    }
+}
